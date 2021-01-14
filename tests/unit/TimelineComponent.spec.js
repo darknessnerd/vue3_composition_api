72fea7fc1677ea52cdb2da6e171f869d91225323
Brunino Criniti <brunino.criniti@konicaminolta.it>
2021-01-14 14:54:52 +0100

    Test fixing

---
+++ tests/unit/TimelineComponent.spec.js
@@ -0,0 +1,71 @@
+import { mount } from '@vue/test-utils';
+import Home from '@/components/Home.vue';
+import flushPromises from 'flush-promises';
+import * as mockData from '@/mocks';
+
+jest.mock('axios',
+  () => ({
+    get: () => ({
+      data: [
+        mockData.todayPost,
+        mockData.thisWeekPost,
+        mockData.thisMonthPost,
+      ],
+    }),
+  }));
+describe('Home', () => {
+  it('renders a loader', () => {
+    const wrapper = mount(Home);
+    expect(wrapper.find('[data-test="progress"]').exists()).toBeTruthy();
+  });
+
+  it('renders 3 times periods', async () => {
+    const wrapper = mount(Home);
+    await flushPromises();
+    expect(wrapper.findAll('[data-test="period"]'))
+      .toHaveLength(3);
+  });
+
+  it('updates the period when clicked', async () => {
+    const wrapper = mount(Home);
+    await flushPromises();
+    const $today = wrapper.findAll('[data-test="period"]')[0];
+    expect($today.classes()).toContain('is-active');
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1];
+    await $thisWeek.trigger('click');
+    expect($today.classes()).not.toContain('is-active');
+    expect($thisWeek.classes()).toContain('is-active');
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2];
+    await $thisMonth.trigger('click');
+    expect($thisWeek.classes()).not.toContain('is-active');
+    expect($thisMonth.classes()).toContain('is-active');
+  });
+
+  it('renders todays post by default', async () => {
+    const wrapper = mount(Home);
+    await flushPromises();
+    expect(wrapper.findAll('[data-test="post"]'))
+      .toHaveLength(1);
+  });
+
+  it('renders change posts', async () => {
+    const wrapper = mount(Home);
+    await flushPromises();
+    expect(wrapper.findAll('[data-test="post"]'))
+      .toHaveLength(1);
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1];
+    await $thisWeek.trigger('click');
+
+    expect(wrapper.findAll('[data-test="post"]'))
+      .toHaveLength(2);
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2];
+    await $thisMonth.trigger('click');
+
+    expect(wrapper.findAll('[data-test="post"]'))
+      .toHaveLength(3);
+  });
+});
