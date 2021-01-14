23e3c1e4ba6aebc7747a0339ac4cd5829dcbd064
Brunino Criniti <brunino.criniti@konicaminolta.it>
2021-01-14 16:31:03 +0100

    post editor

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="is-primary panel">
-    New Post
+    <PostWriter :post="post"/>
+    {{post}}
   </div>
 </template>
 <script>
 import { defineComponent } from 'vue';
+import PostWriter from '@/components/PostWriter.vue';
+import moment from 'moment';
 
 export default defineComponent({
   name: 'NewPost',
+  components: { PostWriter },
+  setup() {
+    const post = {
+      id: -1,
+      title: 'New post',
+      markdown: '## New Post\nEnter your post here...',
+      html: '',
+      created: moment(),
+      authorId: -1,
+    };
+    return {
+      post,
+    };
+  },
 });
 </script>
---
+++ src/components/PostWriter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post Title</div>
+        <div class="control">
+          <input v-model="title" type="text" class="input" />
+          {{ title }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'PostWriter',
+  props: {
+    post: {
+      required: true,
+    },
+  },
+  setup(props) {
+    const title = ref(props.post.title);
+    return {
+      title,
+    };
+  },
+});
+</script>
