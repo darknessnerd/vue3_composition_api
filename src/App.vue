da3f7539bd9d918e5591f42ded86b6838c7406d6
Brunino Criniti <brunino.criniti@konicaminolta.it>
2021-01-18 12:16:21 +0100

    Input generic

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
     ></button>
   </div>
   <section class="section">
+    <FormInput type="text" name="Username" v-model="username" error="aaaa"></FormInput>
+    {{ username }}
     <div class="container">
       <Navbar />
       <router-view />
@@ -17,17 +19,20 @@
 </template>
 
 <script>
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, ref } from 'vue';
 import Navbar from '@/components/Navbar.vue';
+import FormInput from '@/components/FormInput.vue';
 import { useModal } from '@/./useModal';
 
 export default defineComponent({
   name: 'App',
   components: {
     Navbar,
+    FormInput,
   },
   setup() {
     const modal = useModal();
+    const username = ref('username');
     const style = computed(() => ({
       display: modal.visible.value ? 'block' : 'none',
     }));
@@ -35,6 +40,7 @@ export default defineComponent({
     return {
       style,
       modal,
+      username,
     };
   },
 });
---
+++ src/components/FormInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="field">
+    <label class="label" :for="name">{{name}}</label>
+    <div class="control">
+      <input class="input" :type="type" :id="name" @input="handleInput" :value="modelValue" />
+    </div>
+    <p class="help is-danger" v-if="error">{{ error }}</p>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    error: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      required: true,
+      type: String,
+    },
+  },
+  setup(props, ctx) {
+    const handleInput = (event) => {
+      ctx.emit('update:modelValue', event.target.value);
+    };
+    return {
+      handleInput,
+    };
+  },
+});
+</script>
